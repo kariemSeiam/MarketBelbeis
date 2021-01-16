6047f45f2695f1f7c65eed4340b8ca1c06c1dca8
Kariem Seiam <kariemseiam@gmail.com>
2021-01-16 21:21:09 +0200

    update Navigation 1.1

--- app/src/main/java/com/erots/marketbelbeis/ui/auth/login/Login.java
+++ app/src/main/java/com/erots/marketbelbeis/ui/auth/login/Login.java
@@ -7,14 +7,20 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.erots.marketbelbeis.R;
+import com.erots.marketbelbeis.databinding.LoginFragmentBinding;
 
 public class Login extends Fragment {
 
     private LoginViewModel mViewModel;
+    private LoginFragmentBinding binding;
+    private NavController controller;
 
     public static Login newInstance() {
         return new Login();
@@ -23,14 +29,33 @@ public class Login extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.login_fragment, container, false);
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.login_fragment, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        binding.setLifecycleOwner(this);
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        controller = Navigation.findNavController(view);
+
+        binding.tvRestPassLogin.setOnClickListener(v -> {
+            controller.navigate(R.id.action_login_to_restPassword);
+        });
+        binding.tvSignUpLogin.setOnClickListener(v -> {
+            controller.navigate(R.id.action_login_to_signUp);
+        });
+        binding.btnLogin.setOnClickListener(v -> {
+
+        });
+
+    }
 }
--- app/src/main/java/com/erots/marketbelbeis/ui/auth/restpass/RestPassword.java
+++ app/src/main/java/com/erots/marketbelbeis/ui/auth/restpass/RestPassword.java
@@ -7,14 +7,22 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
 
 import com.erots.marketbelbeis.R;
+import com.erots.marketbelbeis.databinding.RestPasswordFragmentBinding;
 
 public class RestPassword extends Fragment {
 
     private RestPasswordViewModel mViewModel;
+    private NavController controller;
+    private RestPasswordFragmentBinding binding;
+    private NavOptions options;
 
     public static RestPassword newInstance() {
         return new RestPassword();
@@ -23,14 +31,28 @@ public class RestPassword extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.rest_password_fragment, container, false);
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.rest_password_fragment, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(RestPasswordViewModel.class);
+        binding.setLifecycleOwner(this);
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        controller = Navigation.findNavController(view);
+        options = new NavOptions.Builder().setPopUpTo(R.id.login, true).build();
+
+        binding.textRestTest.setOnClickListener(v -> {
+            controller.navigate(R.id.action_restPassword_to_login, null, options);
+
+        });
+    }
 }
