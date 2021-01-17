9f59040c97b1e59594b84d57592586c63b32180f
Kariem Seiam <kariemseiam@gmail.com>
2021-01-17 17:02:57 +0200

    Update 1.3

--- app/src/main/java/com/erots/marketbelbeis/ui/main/MainActivity.java
+++ app/src/main/java/com/erots/marketbelbeis/ui/main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.erots.marketbelbeis.ui.main;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,11 +9,23 @@ import com.erots.marketbelbeis.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    NetworkConnection networkConnection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        networkConnection = new NetworkConnection(getApplicationContext());
+        networkConnection.observe(this, aBoolean -> {
+            if (aBoolean) {
+                Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Not Connected", Toast.LENGTH_SHORT).show();
+            }
 
+        });
 
     }
+
+
 }
